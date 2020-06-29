254786137d20f549c87a910a710038395a0bfc9c
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2020-06-29 02:31:57 -0400

    Auto-generated commit

---
+++ assets/preguntasStorage.ts
@@ -0,0 +1,23 @@
+export const preguntasStorage = [
+    {
+        nameAnswer: "Sistema de encuestas",
+        preguntas:[
+            {
+                pregunta: "¿Considera que el sistema esta bien realizado?",
+                tipo: "0"
+            },
+            {
+                pregunta: "¿De el uno al diez que puntaje le daría al código?",
+                tipo: "2"
+            },
+            {
+                pregunta: "¿Recomendaría al programador que lo realizo?",
+                tipo: "0"
+            },
+            {pregunta: "En diseño, ¿con que puntaje lo califica?",
+                tipo: "1"
+            }
+        ],
+        ID: "tn9I7e25367S3D8end43"
+    }
+];
---
+++ assets/respuestasStorage.ts
@@ -0,0 +1,22 @@
+export const respuestasStorage = [
+    {
+        ID: "tn9I7e25367S3D8end43",
+        respuestas: [1, 9, 1, 4]
+    },
+    {
+        ID: "tn9I7e25367S3D8end43",
+        respuestas: [1, 9, 1, 4]
+    },
+    {
+        ID: "tn9I7e25367S3D8end43",
+        respuestas: [1, 9, 1, 4]
+    },
+    {
+        ID: "tn9I7e25367S3D8end43",
+        respuestas: [1, 7, 1, 2]
+    },
+    {
+        ID: "tn9I7e25367S3D8end43",
+        respuestas: [0, 0, 0, 0]
+    }
+];
